70d17a42e5684e1ec09e26900ae0052d736fb7b9
Luis Sumarriva <blackdeath1005@gmail.com>
2013-09-14 01:13:36 -0500

    Primer commit con el proyecto funcionando localmente

---
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAPServices.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            //return "Data Source=.\\SQLEXPRESS;Initial Catalog=BD_Asesores;Integrated Security=SSPI;";
+            return "Data Source=036e9822-d8d1-4590-ab70-a2390059d746.sqlserver.sequelizer.com;Initial Catalog=db036e9822d8d14590ab70a2390059d746;User Id=fvqihuivygnjycpk;Password=H4fyZ6MZcu8nuAoXy5AdTx5akif7tECDDCJr3wLqmUr6XhEFcQ5aNGMj7foQYPEE";
+        }
+
+    }
+}
